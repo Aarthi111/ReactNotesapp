a1dbcf7e71c9c04570ff9184854fb651b240fac5
AarthiRagavan1 <aarthi@mukavai.com>
2025-11-27 14:07:14 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    @tailwind base;
+
+    @tailwind components;
+
+    @tailwind utilities;
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
